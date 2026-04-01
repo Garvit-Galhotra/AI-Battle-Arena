7dfe2856c826d5c4c77239431602cb46f749055c
Garvit-Galhotra <garvitgalhotra1905@gmail.com>
2026-04-01 22:30:17 +0530

    1/4 work for ts and model created

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import useGraph from './services/graph.ai.services.js';
 
 const app = express();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Hello, World!" });
 });
+
+app.post("/use-graph", async (req,res)=>{
+  await useGraph("What is the capital of Inida?")
+})
 
 export default app;
--- src/services/graph.ai.services.ts
+++ src/services/graph.ai.services.ts
@@ -1,26 +1,41 @@
-import type { Message } from "@langchain/core/messages";
-import { StateSchema, MessagesValue, StateGraph, START, END } from "@langchain/langgraph";
 
-type JUDGEMENT = {
-    winner: "solution_1" | "solution_2" ,
-    solution_1_score: number,
-    solution_2_score: number,
-}
+import { HumanMessage } from "@langchain/core/messages";
+import { StateSchema, MessagesValue, StateGraph, START, END, type GraphNode, ReducedValue } from "@langchain/langgraph";
+import {z} from "zod";
 
-type AIBATTLESTATE={
-    message:typeof MessagesValue,
-    solution_1: String,
-    solution_2: String,
-    judgement: JUDGEMENT
-}
 
-const state: AIBATTLESTATE = {
-    message: MessagesValue,
-    solution_1:"",
-    solution_2:"",
-    judgement:{
-        winner: "solution_1",
-        solution_1_score: 0,
-        solution_2_score: 0
+const State = new StateSchema({
+    messages: MessagesValue,
+    solution_1: new ReducedValue( z.string().default(""), {
+        reducer: (current,next)=>{
+            return next
+        }
+    }),
+    solution_2: new ReducedValue( z.string().default(""), {
+        reducer: (current,next)=>{
+            return next
+        }
+    }),
+    judge_Recommendation: new ReducedValue( z.string().default({solution_1: 0, solution_2: 0}), 
+    {
+        reducer: (current,next)=>{
+            return next
+        }
+    }),
+})
+
+const solutionNode:GraphNode<typeof State> = (state:typeof State)=>{
+    console.log(state.messages)
+    return {
+        messages: state.messages[0]
     }
+}
+
+const graph = new StateGraph(State).addNode("solution", solutionNode).addEdge(START, "solution").compile()
+
+export default async function(userMessage: string){
+    const result = await graph.invoke({
+        messages: [new HumanMessage(userMessage)]
+    })
+    return result.messages
 }
